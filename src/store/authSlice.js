630227bf0df03c8fa99432e528f2e3c7f156cfb6
John Muthomi <michenijohn20@gmail.com>
2026-08-20 11:29:36 +0300

    Added login and register constants

--- src/store/authSlice.js
+++ src/store/authSlice.js
@@ -1,3 +1,16 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 import client from "../api/client";
+
+export const login = createAsyncThunk("auth/login", async ({ email, password }) => {
+  const response = await client.post("/auth/login", { email, password });
+  return response.data;
+});
+
+export const register = createAsyncThunk(
+  "auth/register",
+  async ({ email, password }) => {
+    const response = await client.post("/auth/register", { email, password });
+    return response.data;
+  }
+);
